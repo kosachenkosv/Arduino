46191347e4398a42546e71dbd8859895b728c2e2
kosachenkosv <kosachenkosv@yandex.ru>
2026-03-22 22:45:39 +0700

    Add files via upload

---
+++ HTInfraredSeeker/ver2/htirseekerv2_0/htirseekerv2_0.ino
@@ -0,0 +1,164 @@
+/*HiTechnic Infrared Seeker library for Arduino
+  By B. Blechschmidt | August 1, 2013 |
+
+Today I have created a class allowing you to connect a HiTechnic IR Seeker V2, which is originally designed for use with a LEGO Mindstorms NXT robot, to an Arduino Due board with a 9V battery. I struggled a bit with the the different sensor pins but I could finally manage to connect the sensor correctly.
+    смотреть на разъем со стороны контактов
+    
+    white -> battery +9V (Vcc) (в самодельном разъеме красный=белый +9в)
+    black -> Arduino GND
+    red -> battery – (GND)
+    green -> Arduino +3.3V (Vcc)
+    yellow -> Arduino SCL (A5)
+    blue -> Arduino SDA (A4)
+ 
+The following code is executed by the Arduino:
+*/
+
+#include <Wire.h>
+
+/*
+  IRSeeker.ino - A library/class for the HiTechnic IRSeeker V2 infrared sensor.
+  Created by B. Blechschmidt, August 1, 2013.
+  Released into the public domain.
+*/
+
+struct InfraredResult
+{
+  byte Direction;
+  byte Strength;
+};
+
+class InfraredSeeker
+{
+  public:
+    static void Initialize();
+    static boolean Test();
+    static void ReadACRaw(byte* buffer);
+    static void ReadDCRaw(byte* buffer);
+    static InfraredResult ReadAC();
+    static InfraredResult ReadDC();
+    static int DirectionAngle(byte Direction);
+  private:
+    static InfraredResult PopulateValues(byte* buffer);
+    static void ReadValues(byte OffsetAddress, byte* buffer);
+    static const int Address = 0x10 / 2; //Divide by two as 8bit-I2C address is provided
+};
+
+void InfraredSeeker::Initialize()
+{
+  Wire.begin();
+  Wire.beginTransmission(InfraredSeeker::Address);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  while(Wire.available() > 0)
+    Wire.read();
+}
+
+boolean InfraredSeeker::Test()
+{
+  Wire.beginTransmission(InfraredSeeker::Address);
+  Wire.write(0x08);
+  Wire.endTransmission();
+  Wire.requestFrom(InfraredSeeker::Address, 16);
+  char Manufacturer_Model[16];
+  while(Wire.available() < 16);
+  for(byte i=0; i < 16; i++)
+  {
+    Manufacturer_Model[i] = Wire.read();
+  }
+  while(Wire.available() > 0)
+    Wire.read();
+  return strncmp(Manufacturer_Model, "HiTechncNewIRDir", 16)==0;
+}
+
+void InfraredSeeker::ReadValues(byte OffsetAddress, byte* buffer)
+{
+  Wire.beginTransmission(InfraredSeeker::Address);
+  Wire.write(OffsetAddress);
+  Wire.endTransmission();
+  Wire.requestFrom(InfraredSeeker::Address, 6);
+  while(Wire.available() < 6);
+  for(byte i = 0; i < 6; i++)
+  {
+    buffer[i] = Wire.read();
+  }
+  while(Wire.available() > 0)
+    Wire.read();
+}
+
+void InfraredSeeker::ReadACRaw(byte* buffer)
+{
+  ReadValues(0x49, buffer);
+}
+
+void InfraredSeeker::ReadDCRaw(byte* buffer)
+{
+  ReadValues(0x42, buffer);
+}
+
+InfraredResult InfraredSeeker::PopulateValues(byte* buffer)
+{
+  InfraredResult Data;
+  Data.Direction = buffer[0];
+  if(buffer[0] != 0)
+  {
+    if(buffer[0] % 2 == 0)
+    {
+      Data.Strength = (buffer[buffer[0] / 2] + buffer[buffer[0] / 2 + 1]) / 2;
+    }
+    else
+    {
+      Data.Strength = buffer[buffer[0] / 2 + 1];
+    }
+  }
+  else
+  {
+    Data.Strength = 0;
+  }
+  return Data;
+}
+
+InfraredResult InfraredSeeker::ReadAC()  //читает модулированный ИК 1200Гц
+{
+  byte buffer[6];
+  ReadACRaw(buffer);
+  return PopulateValues(buffer);
+}
+
+InfraredResult InfraredSeeker::ReadDC() //читает немодулированный ИК солнце
+{
+  byte buffer[6];
+  ReadDCRaw(buffer);
+  return PopulateValues(buffer);
+}
+
+int DirectionAngle(byte Direction)
+{
+  return Direction * 30 - 150;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("HiTechnic IRSeeker V2");
+  Serial.println();
+  Serial.println();
+  Serial.println("Dir\tAngle\tStrength");
+  Serial.println();
+  InfraredSeeker::Initialize();
+}
+
+void loop()
+{   
+  InfraredResult InfraredBall = InfraredSeeker::ReadAC(); //читает модулированный ИК 1200Гц
+  Serial.print(InfraredBall.Direction);
+  Serial.print("\t");
+  Serial.print(DirectionAngle(InfraredBall.Direction));
+  Serial.print("\t");
+  Serial.print(InfraredBall.Strength);
+  Serial.println();
+  delay(100); //optional
+}
+
+//To download the compressed zip-archive of the library just go to https://blog.blechschmidt.saarland/wp-content/uploads/2013/08/HTInfraredSeeker.zip. This archive can be imported using the “Sketch” menu of your Arduino IDE. Just click “Import library” and choose “Add library…”. Then just #include <HTInfraredSeeker.h> and use it as shown above in the loop- and main function.
+
